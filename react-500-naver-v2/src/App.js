d86644b46532a2e26ed031d8863c0983fc48a05d
preznt <bjw1403@gmail.com>
2023-01-20 01:10:47 +0900

    모달창 화면 디자인 수정

--- react-500-naver-v2/src/App.js
+++ react-500-naver-v2/src/App.js
@@ -4,7 +4,7 @@ import Nav from "./layout/nav";
 
 function App() {
   return (
-    <div className="App w3-container">
+    <div className="App w3-container w3-display-container">
       <header className="App-header w3-padding-24">
         <h1>나의 도서 정보</h1>
         <p>네이버 openAPI 를 연동한 도서정보</p>
--- react-500-naver-v2/src/comps/books/BookModal.js
+++ react-500-naver-v2/src/comps/books/BookModal.js
@@ -19,30 +19,51 @@ const BookModal = ({ open }) => {
   return (
     <div
       className={
-        open ? "w3-card-4 w3-white Modal open" : "w3-card-4 Modal close"
+        open ? "w3-display-container w3-display-middle w3-show" : " w3-hide"
       }
+      style={{ width: "100%", zIndex: 20 }}
     >
-      <header>{bookItem.title}</header>
-      <img src={bookItem.image} />
-      <div className="book detail">
-        <label>ISBN : </label>
-        <input value={bookItem.isbn} />
-        <label>저자 : </label>
-        <input value={bookItem.author} />
-        <label>출판사 : </label>
-        <input value={bookItem.publisher} />
-        <label>가격 : </label>
-        <input value={bookItem.discount} />
-        <label>할인가격 : </label>
-        <input value={bookItem.discount} />
-        <label>링크 : </label>
-        <input value={bookItem.link} />
-        <label>구입일 : </label>
-        <input onChange={onChangeHandler} type="date" />
+      <div
+        className="w3-display-middle w3-white w3-border"
+        style={{ width: "1000px" }}
+      >
+        <header className="w3-green w3-padding-16">
+          <h2>{bookItem.title}</h2>
+        </header>
+        <div className="book detail">
+          <img
+            src={bookItem.image}
+            className="w3-border w3-padding w3-margin"
+            style={{ width: "400px" }}
+          />
+          <div className="w3-left-align w3-margin">
+            <label>ISBN : </label>
+            <input className="w3-input" value={bookItem.isbn} />
+            <label>저자 : </label>
+            <input className="w3-input" value={bookItem.author} />
+            <label>출판사 : </label>
+            <input className="w3-input" value={bookItem.publisher} />
+            <label>가격 : </label>
+            <input className="w3-input" value={bookItem.discount} />
+            <label>할인가격 : </label>
+            <input className="w3-input" value={bookItem.discount} />
+            <label>링크 : </label>
+            <input className="w3-input" value={bookItem.link} />
+            <label>구입일 : </label>
+            <input
+              className="w3-input"
+              onChange={onChangeHandler}
+              type="date"
+            />
+            <button
+              className="w3-button w3-border w3-bar w3-margin-top"
+              onClick={onClickHandler}
+            >
+              추가
+            </button>
+          </div>
+        </div>
       </div>
-      <button className="w3-button w3-border w3-bar" onClick={onClickHandler}>
-        추가
-      </button>
     </div>
   );
 };
